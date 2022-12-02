dd2e66214a91fbc8fd5ff74c7ec97552a53f31f4
Kamil Sobik <kamil.sobik@gmail.com>
2022-12-02 21:49:44 +0100

    add option to input new task

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,10 +1,29 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ApplicationController extends Controller {
-  items = [
+  @tracked items = [
     { isDone: false, label: 'zjeść jajka' },
     { isDone: true, label: 'umyć auto' },
     { isDone: false, label: 'kupić Macbooka' },
     { isDone: true, label: 'napisać apkę' },
   ];
+
+  @action
+  onInputValue(event) {
+    this.newTaskValue = event.target.value;
+  }
+
+  @action
+  newTask(event) {
+    event.preventDefault();
+    this.items = [
+      ...this.items,
+      {
+        isDone: false,
+        label: this.newTaskValue,
+      },
+    ];
+  }
 }
